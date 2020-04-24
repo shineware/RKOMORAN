655fa9c3a6779c6726d1780e1ac43f27af269977
dolpang2 <lkh7689@gmail.com>
2020-04-24 21:04:53 +0900

    Make sure to use UTF8 files

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  options(java.parameters = c("-Xms1g", "-Xmx1g"))
+  options(java.parameters = c("-Xms1g", "-Xmx1g", "-Dfile.encoding=UTF-8"))
 
   libraryPath = "inst/java/KOMORAN-36f59cf.jar"
   rJava::.jpackage(pkgname, morePaths = libraryPath, lib.loc = libname)
